68b4462ba82a602adf57cca935627c7c4d4d6137
Juan Carlos <juanjcsr@gmail.com>
2014-06-16 18:35:27 -0500

    redireccion de comentarios

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,6 +5,7 @@ class CommentsController < ApplicationController
   # GET /comments.json
   def index
     @comments = Comment.all
+    @comment = Comment.new
   end
 
   # GET /comments/1
@@ -28,7 +29,7 @@ class CommentsController < ApplicationController
 
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
+        format.html { redirect_to root_path, notice: 'Comment was successfully created.' }
         format.json { render :show, status: :created, location: @comment }
       else
         format.html { render :new }
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
   #get 'static_pages/index'
 
-  resources :comments
+  resources :comments, only: [:new, :create]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'static_pages#index'
+
+  root 'comments#new'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
